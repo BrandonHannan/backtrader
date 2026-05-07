fd93899995b93b2129a3e21536cde3821a81d376
Brandon Hannan <bhannan@dingo.com>
2026-05-07 15:05:24 +1000

    Optimal Base Case

--- BackTrader/StrategyRunner/DowATRStrategy/DowATRBaseCase.h
+++ BackTrader/StrategyRunner/DowATRStrategy/DowATRBaseCase.h
@@ -17,10 +17,10 @@ struct DowBaseCase {
     int signalLookback = 10;
     TrendMode trendMode = TrendMode::THREE_POINT;
     TrendLineMode trendLineMode = TrendLineMode::MINIMUM;
-    double agreementThreshold = 0.7;
-    double relativeMomentumThreshold = 2.0;
+    double agreementThreshold = 0.5;
+    double relativeMomentumThreshold = 0.5;
     double breakoutConfluenceThreshold = 0.8;
-    double ecosystemVolatilityThreshold = 1.5;
+    double ecosystemVolatilityThreshold = 0.5;
 };
 
 #endif
